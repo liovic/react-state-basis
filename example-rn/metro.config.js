083a70674678019bed514233fc73b3eb0cff77f6
Petar Liovic <petarliovic1@gmail.com>
2025-12-29 19:30:52 +0100

    chore: update examples to v0.1.4

--- example-rn/metro.config.js
+++ example-rn/metro.config.js
@@ -1,20 +1,2 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
-
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '..');
-
-const config = getDefaultConfig(projectRoot);
-
-config.watchFolders = [workspaceRoot];
-
-config.resolver.extraNodeModules = {
-  'react': path.resolve(projectRoot, 'node_modules/react'),
-  'react-native': path.resolve(projectRoot, 'node_modules/react-native'),
-};
-
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-];
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
